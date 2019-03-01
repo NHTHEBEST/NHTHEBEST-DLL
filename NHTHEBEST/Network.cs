e71863d69e787f586248182b25276e783dc59f95
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2019-03-01 17:42:54 +1300

    Update Network.cs

--- NHTHEBEST/Network.cs
+++ NHTHEBEST/Network.cs
@@ -43,7 +43,7 @@ namespace NHTHEBEST
                     streamr = new StreamReader(stream);
                 }
             }
-            public void Dispose()
+            public void Dispose() // Noncompliant
             {
                 try
                 {
